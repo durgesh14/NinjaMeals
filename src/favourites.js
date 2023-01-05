28c1ad0e8aa5bba3f5ac4d8a5f338e66783b41cc
durgesh14 <34915741+durgesh14@users.noreply.github.com>
2023-01-05 12:40:53 +0530

    Added fvrts page(local storage code).

---
+++ src/favourites.js
@@ -0,0 +1,66 @@
+var container = document.getElementById("container");
+
+window.onload = function () {
+  // Get data from local storage
+  for (let key in localStorage) {
+    if (!isNaN(parseInt(key))) {
+      console.log(key);
+
+      fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + key)
+        .then((res) => res.json()) // Convert the response to JSON format
+        .then((data) => showContent(data)); // Pass the JSON data to the showContent() function
+    } //end if
+
+
+  }
+};
+
+const showContent = (data) => {
+  console.log(data.meals[0].strMeal);
+  let imgLink = data.meals[0].strMealThumb;
+  let desc = data.meals[0].strMeal;
+  let tags = data.meals[0].strTags;
+
+  // Create an img element
+  var img = document.createElement("img");
+  img.src = imgLink;
+
+  // Create a p element
+  var desciption = document.createElement("p");
+  desciption.innerHTML = desc;
+
+  // Create a p element
+  var tagele = document.createElement("p");
+  if (tags != null) {
+    tagele.innerHTML = "Tags: " + tags;
+  } else {
+    tagele.innerHTML = "No Tags";
+  }
+
+  const card = document.createElement("div");
+  card.className = "Cards";
+
+  const info = document.createElement("div");
+  info.className = "info";
+
+  const remove = document.createElement("span");
+  remove.className = "remove";
+
+  remove.innerHTML = '<img src="./assests/remove.svg" alt="My SVG Image">';
+
+  // Append the elements to the document
+  card.appendChild(img);
+  card.appendChild(info);
+
+  info.appendChild(desciption);
+  info.appendChild(tagele);
+  info.appendChild(remove);
+
+  container.appendChild(card);
+
+  remove.addEventListener('click', function(){
+    localStorage.removeItem(data.meals[0].idMeal);
+    card.remove();
+  })
+  
+};
--- src/script.js
+++ src/script.js
@@ -1,48 +1,85 @@
 function searchFunction() {
-    // Get the value of the search field
-    var searchValue = document.getElementById("search").value;
-    console.log( document.querySelector(".meal"));
+  // Get the value of the search field
+  var searchValue = document.getElementById("search").value;
+  // console.log( document.querySelector(".meal"));
 
-    while(document.querySelector(".meal")){
-        var div = document.querySelector(".meal");  // Get the div element
-        var parent = div.parentNode;  // Get the parent element of the div
-        parent.removeChild(div);
-      }
-  
- //console.log( document.querySelector("meal"));
-    // Do something with the search value (e.g. send an AJAX request to a server)
-    // ...
-
-  //  fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=' + searchValue)
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchValue)
-    .then(res=>res.json())
-    .then(data=> searchResults(data))
-    .catch(error => {
-        console.error(error);  // Log the error to the console
-      });
-    
-    
-    
-    const searchResults = (data) =>{
-        console.log(data);
-        if(data.meals == null){
-            alert("No search found!!")
-            
-        }else{
-    const itemsdiv = document.getElementById('item-list');
-    
-    
-    data.meals.forEach(meal=>{
-    
-        const food = document.createElement('div');
-    
-        food.className = 'meal';
-        const foodName = `<h3>${meal.strMeal}</h3>`;
-        food.innerHTML = foodName;
-        itemsdiv.appendChild(food);
-    });
-    }
+  //Loop to remove the list items to only show relevant search result.
+  while (document.querySelector(".meal")) {
+    var div = document.querySelector(".meal"); // Get the div element
+    var parent = div.parentNode; // Get the parent element of the div
+    parent.removeChild(div);
   }
+
+  // The fetch() function returns a Promise that resolves with the response from the server
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchValue)
+    .then((res) => res.json()) // Convert the response to JSON format
+    .then((data) => searchResults(data)) // Pass the JSON data to the searchResults() function
+    .catch((error) => {
+      console.error(error); // Log the error to the console
+    });
+
+  // The searchResults() function takes a list of data(All the meals) and displays them on the page
+  const searchResults = (data) => {
+    // console.log(data);
+    if (data.meals == null) {
+      //Check if searched meal is not found
+      alert("No search found!!");
+    } else {
+      //If meal is found
+
+      // Get the element where the meals will be displayed
+      const itemsdiv = document.getElementById("item-list");
+
+
+
+      // Loop through the list of meals
+      data.meals.forEach((meal) => {
+        // Create a new 'div' element for the meal
+        const food = document.createElement("div");
+        const favourite = document.createElement("button");
+
+        food.className = "meal"; // Add a class name to the div element
+        favourite.className = "meal fvrt-btn";
+
+        const foodName = `<h3>${meal.strMeal}</h3>`; // Create the HTML content for the meal
+        food.innerHTML = foodName; // Set the inner HTML of the div element to the HTML content
+        if(!localStorage.getItem(meal.idMeal))
+        favourite.innerHTML = "Add to favourite";
+        else{
+            favourite.innerHTML = "Remove from favourite";
+        }
+        itemsdiv.appendChild(food); // Append the div element to the itemsdiv element
+        itemsdiv.appendChild(favourite);
+
+        // const fvrtbtn = document.querySelectorAll(".fvrt-btn");
+
+       // fvrtbtn.forEach((button) => {
+        favourite.addEventListener("click", (event) => {
+           // const h3 = event.target.previousElementSibling;
+           if(!localStorage.getItem(meal.idMeal)){
+            favourite.innerHTML = "Remove from favourite";
+            localStorage.setItem(meal.idMeal, meal.strMeal);
+           }
+           
+           else{
+               
+               favourite.innerHTML = "Add to favourite";
+               localStorage.removeItem(meal.idMeal);
+           }
+          
+
+
+          });
+
+
+          //Event listner on the meal name
+          food.addEventListener('click', function(){
+            window.location.href = './detailsPage.html?id='+meal.idMeal;
+          });
+        //});
+      });
+    }
+  };
 }
 
 
